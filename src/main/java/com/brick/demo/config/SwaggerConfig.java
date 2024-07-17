e0eab6e6dd3e67c6ac28ec3e87f0e57e411614e1
hunnypooh <jiwon2461999@gmail.com>
2024-07-17 23:08:28 +0900

    fix: swagger https로도 요청 가능
    - https://break-brick.kro.kr/, http://localhost:8080/ 두개로 요청 선택해서 보낼 수 있게 설정

--- src/main/java/com/brick/demo/config/SwaggerConfig.java
+++ src/main/java/com/brick/demo/config/SwaggerConfig.java
@@ -3,6 +3,9 @@ package com.brick.demo.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,15 +13,20 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
   @Bean
   public OpenAPI openAPI() {
+    List<Server> servers = new ArrayList<>();
+    servers.add(new Server().url("https://break-brick.kro.kr/"));
+    servers.add(new Server().url("http://localhost:8080/"));
+
     return new OpenAPI()
         .components(new Components())
-        .info(apiInfo());
+        .info(apiInfo())
+        .servers(servers);
   }
 
   private Info apiInfo() {
     return new Info()
         .title("Brick API 명세")
-        .description("Specification")
+        .description("잘못된 부분이나 오류 발생 시 바로 말씀해주세요.") // 문서 설명
         .version("0.0.1");
   }
 }
