9e1ac84ebb91c3dfb924b119bed6b06d789dea49
hunnypooh <jiwon2461999@gmail.com>
2024-07-23 23:45:34 +0900

    feat: CustomException 생성자 추가 [#10]
    ErrorDetail 없이 Http 메서드와 스트링 타입 메세지만으로도 CustomException 생성 가능하도록 수정

--- src/main/java/com/brick/demo/common/CustomException.java
+++ src/main/java/com/brick/demo/common/CustomException.java
@@ -6,22 +6,29 @@ import org.springframework.http.HttpStatus;
 @Getter
 public class CustomException extends RuntimeException {
 
-  private final String code;
-  private final HttpStatus httpStatus;
-  private final String additionalInfo;
+	private final String code;
+	private final HttpStatus httpStatus;
+	private final String additionalInfo;
 
-  public CustomException(ErrorDetails errorDetails) {
-    super(errorDetails.getMessage());
-    this.code = errorDetails.getCode();
-    this.httpStatus = errorDetails.getStatus();
-    this.additionalInfo = null;
-  }
+	public CustomException(ErrorDetails errorDetails) {
+		super(errorDetails.getMessage());
+		this.code = errorDetails.getCode();
+		this.httpStatus = errorDetails.getStatus();
+		this.additionalInfo = null;
+	}
+
+	public CustomException(HttpStatus httpStatus, String message) {
+		super(message);
+		this.code = null;
+		this.httpStatus = httpStatus;
+		this.additionalInfo = null;
+	}
 
 
-  public CustomException(ErrorDetails errorDetails, String additionalInfo) {
-    super(errorDetails.getMessage());
-    this.code = errorDetails.getCode();
-    this.httpStatus = errorDetails.getStatus();
-    this.additionalInfo = additionalInfo;
-  }
+	public CustomException(ErrorDetails errorDetails, String additionalInfo) {
+		super(errorDetails.getMessage());
+		this.code = errorDetails.getCode();
+		this.httpStatus = errorDetails.getStatus();
+		this.additionalInfo = additionalInfo;
+	}
 }
