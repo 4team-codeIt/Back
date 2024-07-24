4c5a6ca61740a29778fe3997fcd2bac5533bd14f
kimyu0218 <serena35@ewhain.net>
2024-07-24 18:24:22 +0900

    feat: Social entity에 대한 레포지토리 작성 [#11]

--- src/main/java/com/brick/demo/DemoApplication.java
+++ src/main/java/com/brick/demo/DemoApplication.java
@@ -5,14 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableJpaRepositories
 @Import(ApplicationConfig.class)
 public class DemoApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(DemoApplication.class, args);
   }
-
 }
