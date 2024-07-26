da22626f295c525207e13755cd484e8f4071d4d5
kimyu0218 <serena35@ewhain.net>
2024-07-27 02:32:19 +0900

    refactor: 테스트에서 사용하는 상수 분리 [#11]

--- src/test/java/com/brick/demo/social/repository/SocialDetailRepositoryTest.java
+++ src/test/java/com/brick/demo/social/repository/SocialDetailRepositoryTest.java
@@ -1,15 +1,14 @@
 package com.brick.demo.social.repository;
 
+import static com.brick.demo.fixture.AccountFixture.ACCOUNT;
+import static com.brick.demo.fixture.SocialFixture.PLACE;
+import static com.brick.demo.fixture.SocialFixture.TODAY_SOCIAL;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.brick.demo.auth.entity.Account;
 import com.brick.demo.auth.repository.AccountRepository;
-import com.brick.demo.social.dto.ParticipantCount;
-import com.brick.demo.social.dto.Place;
-import com.brick.demo.social.dto.SocialCreateRequest;
 import com.brick.demo.social.entity.Social;
 import com.brick.demo.social.entity.SocialDetail;
-import java.time.LocalDateTime;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -27,21 +26,14 @@ class SocialDetailRepositoryTest {
 
   @Test
   void 모임_아이디에_해당하는_모임_상세_정보를_조회한다() {
-    Account account =
-        accountRepository.save(
-            Account.builder().name("tester").email("test@example.com").password("passwd").build());
+    Account account = accountRepository.save(ACCOUNT);
 
-    Place place = new Place("서울시 강남구 테헤란로 123", "456동 789호", 37.5665, 126.978);
-    ParticipantCount participantCount = new ParticipantCount(2, 5);
-    SocialCreateRequest dto =
-        new SocialCreateRequest(
-            "오늘 모임", "오늘 진행하는 모임입니다.", LocalDateTime.now(), participantCount, place);
-    Social social = Social.save(account, dto);
+    Social social = Social.save(account, TODAY_SOCIAL);
     socialRepository.save(social);
 
-    socialDetailRepository.save(SocialDetail.save(social, dto));
+    socialDetailRepository.save(SocialDetail.save(social, TODAY_SOCIAL));
     SocialDetail detail = socialDetailRepository.findBySocialId(social.getId()).get();
 
-    assertThat(detail.getGeoLocation()).isEqualTo("37.5665 126.978");
+    assertThat(detail.getGeoLocation()).isEqualTo(PLACE.latitude() + " " + PLACE.longitude());
   }
 }
--- src/test/java/com/brick/demo/social/repository/SocialRepositoryTest.java
+++ src/test/java/com/brick/demo/social/repository/SocialRepositoryTest.java
@@ -1,14 +1,17 @@
 package com.brick.demo.social.repository;
 
+import static com.brick.demo.fixture.AccountFixture.ACCOUNT;
+import static com.brick.demo.fixture.SocialFixture.FUTURE_SOCIAL;
+import static com.brick.demo.fixture.SocialFixture.PAST_SOCIAL;
+import static com.brick.demo.fixture.SocialFixture.TODAY_SOCIAL;
+import static com.brick.demo.fixture.SocialFixture.TOMORROW;
+import static com.brick.demo.fixture.SocialFixture.YESTERDAY;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import com.brick.demo.auth.entity.Account;
 import com.brick.demo.auth.repository.AccountRepository;
-import com.brick.demo.social.dto.ParticipantCount;
-import com.brick.demo.social.dto.Place;
-import com.brick.demo.social.dto.SocialCreateRequest;
 import com.brick.demo.social.entity.Social;
-import java.time.LocalDateTime;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -26,27 +29,11 @@ class SocialRepositoryTest {
 
   @BeforeEach
   void 테스트_데이터_삽입() {
-    Account account =
-        accountRepository.save(
-            Account.builder().name("tester").email("test@example.com").password("passwd").build());
+    Account account = accountRepository.save(ACCOUNT);
 
-    LocalDateTime now = LocalDateTime.now();
-    Place place = new Place("서울시 강남구 테헤란로 123", "456동 789호", 37.5665, 126.978);
-    ParticipantCount participantCount = new ParticipantCount(2, 5);
-
-    SocialCreateRequest dto1 =
-        new SocialCreateRequest("오늘 모임", "오늘 진행하는 모임입니다.", now, participantCount, place);
-    Social social1 = Social.save(account, dto1);
-
-    SocialCreateRequest dto2 =
-        new SocialCreateRequest(
-            "한달 후 모임", "한달 후에 진행할 모임입니다.", now.plusMonths(1), participantCount, place);
-    Social social2 = Social.save(account, dto2);
-
-    SocialCreateRequest dto3 =
-        new SocialCreateRequest(
-            "한달 전 모임", "한달 전에 진행했던 모임입니다.", now.minusMonths(1), participantCount, place);
-    Social social3 = Social.save(account, dto3);
+    Social social1 = Social.save(account, TODAY_SOCIAL);
+    Social social2 = Social.save(account, FUTURE_SOCIAL);
+    Social social3 = Social.save(account, PAST_SOCIAL);
 
     socialRepository.save(social1);
     socialRepository.save(social2);
@@ -57,30 +44,31 @@ class SocialRepositoryTest {
   void 모임_일자_내림차순으로_모임을_조회한다() {
     List<Social> socials = socialRepository.findAllByOrderByGatheringDateDesc();
 
-    assertThat(socials).hasSize(3);
-    assertThat(socials.get(0).getName()).isEqualTo("한달 후 모임");
-    assertThat(socials.get(2).getName()).isEqualTo("한달 전 모임");
+    assertAll(
+        () -> assertThat(socials).hasSize(3),
+        () -> assertThat(socials.get(0).getName()).isEqualTo(FUTURE_SOCIAL.name()),
+        () -> assertThat(socials.get(2).getName()).isEqualTo(PAST_SOCIAL.name()));
   }
 
   @Test
   void 내일_기준_이전의_모임을_조회한다() {
-    LocalDateTime tomorrow = LocalDateTime.now().plusDays(1);
     List<Social> socials =
-        socialRepository.findAllByGatheringDateBeforeOrderByGatheringDateDesc(tomorrow);
+        socialRepository.findAllByGatheringDateBeforeOrderByGatheringDateDesc(TOMORROW);
 
-    assertThat(socials).hasSize(2);
-    assertThat(socials.get(0).getName()).isEqualTo("오늘 모임");
-    assertThat(socials.get(1).getName()).isEqualTo("한달 전 모임");
+    assertAll(
+        () -> assertThat(socials).hasSize(2),
+        () -> assertThat(socials.get(0).getName()).isEqualTo(TODAY_SOCIAL.name()),
+        () -> assertThat(socials.get(1).getName()).isEqualTo(PAST_SOCIAL.name()));
   }
 
   @Test
   void 어제_기준_이후의_모임을_조회한다() {
-    LocalDateTime yesterday = LocalDateTime.now().minusDays(1);
     List<Social> socials =
-        socialRepository.findAllByGatheringDateAfterOrderByGatheringDateDesc(yesterday);
+        socialRepository.findAllByGatheringDateAfterOrderByGatheringDateDesc(YESTERDAY);
 
-    assertThat(socials).hasSize(2);
-    assertThat(socials.get(0).getName()).isEqualTo("한달 후 모임");
-    assertThat(socials.get(1).getName()).isEqualTo("오늘 모임");
+    assertAll(
+        () -> assertThat(socials).hasSize(2),
+        () -> assertThat(socials.get(0).getName()).isEqualTo(FUTURE_SOCIAL.name()),
+        () -> assertThat(socials.get(1).getName()).isEqualTo(TODAY_SOCIAL.name()));
   }
 }
