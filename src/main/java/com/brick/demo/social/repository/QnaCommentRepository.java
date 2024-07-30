3392b505d7af28b851aef1294069424b7a0c8da0
hunnypooh <jiwon2461999@gmail.com>
2024-07-30 21:20:31 +0900

    feat: /socials/{}/qnas/{}/comments 페이지네이션 [#21]

--- src/main/java/com/brick/demo/social/repository/QnaCommentRepository.java
+++ src/main/java/com/brick/demo/social/repository/QnaCommentRepository.java
@@ -1,10 +1,11 @@
 package com.brick.demo.social.repository;
 
+import com.brick.demo.common.repository.CommonPaginationRepository;
 import com.brick.demo.social.entity.Qna;
 import com.brick.demo.social.entity.QnaComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface QnaCommentRepository extends JpaRepository<QnaComment, Long> {
-
-
+public interface QnaCommentRepository extends JpaRepository<QnaComment, Long>,
+		CommonPaginationRepository<QnaComment, Long> {
+	
 }
--- src/main/java/com/brick/demo/social/service/QnaCommentService.java
+++ src/main/java/com/brick/demo/social/service/QnaCommentService.java
@@ -7,17 +7,24 @@ import com.brick.demo.auth.repository.AccountRepository;
 import com.brick.demo.common.CustomException;
 import com.brick.demo.common.ErrorDetails;
 import com.brick.demo.security.CustomUserDetails;
+import com.brick.demo.social.dto.PaginationIdResponse;
 import com.brick.demo.social.dto.QnaCommentPatchDto;
 import com.brick.demo.social.dto.QnaCommentRequestDto;
 import com.brick.demo.social.dto.QnaCommentResponseDto;
+import com.brick.demo.social.dto.QnaResponseDto;
 import com.brick.demo.social.entity.Qna;
 import com.brick.demo.social.entity.QnaComment;
 import com.brick.demo.social.repository.QnaCommentRepository;
 import com.brick.demo.social.repository.QnaRepository;
 import jakarta.transaction.Transactional;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -30,6 +37,25 @@ public class QnaCommentService {
 	private final QnaCommentRepository qnaCommentRepository;
 	private final AccountRepository accountRepository;
 	private final QnaRepository qnaRepository;
+
+	@Transactional
+	public PaginationIdResponse getCommentsByQnaId(Long qnaId, Long cursor, int limit) {
+		PageRequest pageable = PageRequest.of(0, limit);
+		Map<String, Object> conditions = new HashMap<>();
+		conditions.put("qna.id", qnaId);
+
+		List<QnaComment> comments = qnaCommentRepository.findByCursorAndOrderByIdAsc(cursor, pageable,
+				conditions);
+		boolean hasNext = comments.size() > limit;
+		if (hasNext) {
+			comments = comments.subList(0, limit); // 필요한 만큼만 반환
+		}
+		List<QnaCommentResponseDto> commentResponseDtos = comments.stream()
+				.map(QnaCommentResponseDto::new)
+				.collect(Collectors.toList());
+		Long nextCursor = hasNext ? comments.get(limit - 1).getId() : null;
+		return new PaginationIdResponse(nextCursor, commentResponseDtos);
+	}
 
 	@Transactional
 	public QnaCommentResponseDto create(Long qnaId, QnaCommentRequestDto dto) {
