4510453164c2b2ffc29011b1c87dcd3c8567dfba
kimyu0218 <serena35@ewhain.net>
2024-08-01 05:30:37 +0900

    fix: [] -> List

--- src/main/java/com/brick/demo/social/dto/SocialUpdateRequest.java
+++ src/main/java/com/brick/demo/social/dto/SocialUpdateRequest.java
@@ -3,6 +3,7 @@ package com.brick.demo.social.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
+import java.util.List;
 
 public record SocialUpdateRequest(
     @Schema(description = "모임 이름", requiredMode = Schema.RequiredMode.REQUIRED)
@@ -15,6 +16,6 @@ public record SocialUpdateRequest(
     @Valid Place place,
 
     @Schema(description = "이미지 URL 배열")
-    String[] imageUrls
+    List<String> imageUrls
 ) {
 }
