9443bb2606f0dfa6f747b2bff79e07d60d84052a
kimyu0218 <serena35@ewhain.net>
2024-08-01 05:25:23 +0900

    remove: 불필요한 stack 출력 삭제

--- src/main/java/com/brick/demo/common/GlobalExceptionHandler.java
+++ src/main/java/com/brick/demo/common/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.brick.demo.common;
 
-import com.brick.demo.auth.controller.AuthController;
 import jakarta.validation.ConstraintViolationException;
 import java.util.HashMap;
 import java.util.Map;
@@ -20,60 +19,65 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @AllArgsConstructor
 class ErrorResponse {
 
-	private final String code;
-	private final String message;
-
+  private final String code;
+  private final String message;
 }
 
 @RestControllerAdvice()
 public class GlobalExceptionHandler {
 
-	private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+  private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
-	@ExceptionHandler(CustomException.class)
-	public ResponseEntity<ErrorResponse> handleCustomException(CustomException ex) {
-		logger.error("CustomException 발생: Code = {}, Message = {}", ex.getCode(), ex.getMessage(),
-				ex);
+  @ExceptionHandler(CustomException.class)
+  public ResponseEntity<ErrorResponse> handleCustomException(CustomException ex) {
+    logger.error("CustomException 발생: Code = {}, Message = {}", ex.getCode(), ex.getMessage());
 
-		ErrorResponse errorResponse = new ErrorResponse(ex.getCode(), ex.getMessage());
-		return new ResponseEntity<>(errorResponse, ex.getHttpStatus());
-	}
+    ErrorResponse errorResponse = new ErrorResponse(ex.getCode(), ex.getMessage());
+    return new ResponseEntity<>(errorResponse, ex.getHttpStatus());
+  }
 
+  @ExceptionHandler(MethodArgumentNotValidException.class) // @RequestBody에서 유효성 검사가 실패할때
+  public ResponseEntity<Map<String, String>> handleValidationExceptions(
+      MethodArgumentNotValidException ex) {
+    Map<String, String> errors = new HashMap<>();
+    ex.getBindingResult()
+        .getAllErrors()
+        .forEach(
+            error -> {
+              String fieldName = ((FieldError) error).getField();
+              String errorMessage = error.getDefaultMessage();
+              errors.put(fieldName, errorMessage);
+            });
+    return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+  }
 
-	@ExceptionHandler(MethodArgumentNotValidException.class) //@RequestBody에서 유효성 검사가 실패할때
-	public ResponseEntity<Map<String, String>> handleValidationExceptions(
-			MethodArgumentNotValidException ex) {
-		Map<String, String> errors = new HashMap<>();
-		ex.getBindingResult().getAllErrors().forEach(error -> {
-			String fieldName = ((FieldError) error).getField();
-			String errorMessage = error.getDefaultMessage();
-			errors.put(fieldName, errorMessage);
-		});
-		return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
-	}
+  @ExceptionHandler(ConstraintViolationException.class)
+  // @RequestParam, @PathVariable, 메서드 파라미터에서 유효성 검사가 실패할때
+  public ResponseEntity<Map<String, String>> handleConstraintViolationException(
+      ConstraintViolationException ex) {
+    Map<String, String> errors = new HashMap<>();
+    ex.getConstraintViolations()
+        .forEach(
+            violation -> {
+              String fieldName = violation.getPropertyPath().toString();
+              String errorMessage = violation.getMessage();
+              errors.put(fieldName, errorMessage);
+            });
+    return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+  }
 
-	@ExceptionHandler(ConstraintViolationException.class)
-	//@RequestParam, @PathVariable, 메서드 파라미터에서 유효성 검사가 실패할때
-	public ResponseEntity<Map<String, String>> handleConstraintViolationException(
-			ConstraintViolationException ex) {
-		Map<String, String> errors = new HashMap<>();
-		ex.getConstraintViolations().forEach(violation -> {
-			String fieldName = violation.getPropertyPath().toString();
-			String errorMessage = violation.getMessage();
-			errors.put(fieldName, errorMessage);
-		});
-		return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
-	}
-	
-	@ExceptionHandler(MissingServletRequestParameterException.class)
-	public ResponseEntity<ErrorResponse> handleMissingServletRequestParameterException(
-			MissingServletRequestParameterException ex) {
-		logger.error("MissingServletRequestParameterException 발생: Parameter = {}, Message = {}",
-				ex.getParameterName(), ex.getMessage(), ex);
+  @ExceptionHandler(MissingServletRequestParameterException.class)
+  public ResponseEntity<ErrorResponse> handleMissingServletRequestParameterException(
+      MissingServletRequestParameterException ex) {
+    logger.error(
+        "MissingServletRequestParameterException 발생: Parameter = {}, Message = {}",
+        ex.getParameterName(),
+        ex.getMessage(),
+        ex);
 
-		String code = "MISSING_PARAMETER";
-		String message = String.format("필수 파라미터 '%s'가 누락되었습니다.", ex.getParameterName());
-		ErrorResponse errorResponse = new ErrorResponse(code, message);
-		return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
-	}
+    String code = "MISSING_PARAMETER";
+    String message = String.format("필수 파라미터 '%s'가 누락되었습니다.", ex.getParameterName());
+    ErrorResponse errorResponse = new ErrorResponse(code, message);
+    return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+  }
 }
