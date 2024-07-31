6d1cbac528100b4691012048fc8ffc0339e8ee1a
kimyu0218 <serena35@ewhain.net>
2024-08-01 05:27:13 +0900

    refactor: 기획 변경으로 인한 필드 추가

--- src/main/java/com/brick/demo/social/dto/Participant.java
+++ src/main/java/com/brick/demo/social/dto/Participant.java
@@ -1,9 +1,14 @@
 package com.brick.demo.social.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 public record Participant(
+    @Schema(description = "모임 아이디", requiredMode = RequiredMode.REQUIRED)
+    @NotNull Long id,
+
     @Schema(description = "모임 참가자의 이름", requiredMode = Schema.RequiredMode.REQUIRED)
     @NotEmpty  String name,
 
