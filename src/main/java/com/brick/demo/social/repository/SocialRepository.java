b471f0a92cae50d79c8a4eec73a470a64c4d574d
kimyu0218 <serena35@ewhain.net>
2024-08-01 19:11:46 +0900

    fix: 모임 정렬 기준 수정

--- src/main/java/com/brick/demo/social/repository/SocialRepository.java
+++ src/main/java/com/brick/demo/social/repository/SocialRepository.java
@@ -15,12 +15,35 @@ public interface SocialRepository extends JpaRepository<Social, Long> {
 
   List<Social> findAllByGatheringDateBeforeOrderByGatheringDateDesc(final LocalDateTime date);
 
+  List<Social> findAllByGatheringDateBeforeOrderByCreatedAtDesc(final LocalDateTime date);
+
   List<Social> findAllByGatheringDateAfterOrderByGatheringDateDesc(final LocalDateTime date);
+
+  List<Social> findAllByGatheringDateAfterOrderByCreatedAtDesc(final LocalDateTime date);
 
   List<Social> findAllByCanceledTrueOrderByCreatedAtDesc();
 
   List<Social> findAllByOwnerEntityIdOrderByCreatedAtDesc(final Long ownerEntityId);
 
   @Query("SELECT s FROM Social s ORDER BY SIZE(s.participants) DESC")
-  List<Social> findAllOrderByPopularityDesc();
+  List<Social> findAllByOrderByPopularityDesc();
+
+  List<Social> findAllByIdInOrderByCreatedAtDesc(final List<Long> ids);
+
+  List<Social> findAllByIdInOrderByGatheringDateDesc(final List<Long> ids);
+
+  List<Social> findAllByIdInAndGatheringDateBeforeOrderByGatheringDateDesc(
+      final List<Long> ids, final LocalDateTime date);
+
+  List<Social> findAllByIdInAndGatheringDateBeforeOrderByCreatedAtDesc(
+      final List<Long> ids, final LocalDateTime date);
+
+  List<Social> findAllByIdInAndGatheringDateAfterOrderByGatheringDateDesc(
+      final List<Long> ids, final LocalDateTime date);
+
+  List<Social> findAllByIdInAndGatheringDateAfterOrderByCreatedAtDesc(
+      final List<Long> ids, final LocalDateTime date);
+
+  @Query("SELECT s FROM Social s WHERE s.id IN :ids ORDER BY SIZE(s.participants) DESC")
+  List<Social> findAllByIdInOrderByPopularityDesc(final List<Long> ids);
 }
--- src/main/java/com/brick/demo/social/service/SocialService.java
+++ src/main/java/com/brick/demo/social/service/SocialService.java
@@ -6,7 +6,6 @@ import com.brick.demo.auth.entity.Account;
 import com.brick.demo.auth.repository.AccountRepository;
 import com.brick.demo.common.CustomException;
 import com.brick.demo.common.ErrorDetails;
-import com.brick.demo.security.CustomUserDetails;
 import com.brick.demo.social.dto.SocialCreateRequest;
 import com.brick.demo.social.dto.SocialCreateResponse;
 import com.brick.demo.social.dto.SocialDetailResponse;
@@ -24,8 +23,6 @@ import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -33,105 +30,128 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class SocialService {
 
-	private static final Logger log = LoggerFactory.getLogger(SocialService.class);
-	private final SocialRepository socialRepository;
-	private final SocialDetailRepository socialDetailRepository;
-	private final SocialParticipantRepository socialParticipantRepository;
-	private final AccountRepository accountRepository;
+  private static final Logger log = LoggerFactory.getLogger(SocialService.class);
+  private final SocialRepository socialRepository;
+  private final SocialDetailRepository socialDetailRepository;
+  private final SocialParticipantRepository socialParticipantRepository;
+  private final AccountRepository accountRepository;
 
-	@Transactional
-	public List<SocialResponse> selectSocials(final String filterBy, final String orderBy) {
-		return findAllSocials(filterBy, orderBy).stream()
-				.map(SocialResponse::fromEntity)
-				.collect(Collectors.toList());
-	}
+  @Transactional
+  public List<SocialResponse> selectSocials(final String filterBy, final String orderBy) {
+    return findAllSocials(filterBy, orderBy).stream()
+        .map(SocialResponse::fromEntity)
+        .collect(Collectors.toList());
+  }
 
-	@Transactional
-	public SocialResponse selectSocialById(Long id) throws CustomException {
-		Social social =
-				socialRepository
-						.findById(id)
-						.orElseThrow(() -> new CustomException(ErrorDetails.SOCIAL_NOT_FOUND));
+  @Transactional
+  public SocialResponse selectSocialById(Long id) throws CustomException {
+    Social social =
+        socialRepository
+            .findById(id)
+            .orElseThrow(() -> new CustomException(ErrorDetails.SOCIAL_NOT_FOUND));
 
-		return SocialResponse.fromEntity(social);
-	}
+    return SocialResponse.fromEntity(social);
+  }
 
-	@Transactional
-	public SocialCreateResponse createSocial(final SocialCreateRequest dto) {
-		Account account = getCurrentAccount(accountRepository);
-		Social social = socialRepository.save(new Social(account, dto));
-		SocialDetail detail = socialDetailRepository.save(new SocialDetail(social, dto));
+  @Transactional
+  public SocialCreateResponse createSocial(final SocialCreateRequest dto) {
+    Account account = getCurrentAccount(accountRepository);
+    Social social = socialRepository.save(new Social(account, dto));
+    SocialDetail detail = socialDetailRepository.save(new SocialDetail(social, dto));
 
-		socialParticipantRepository.save(new SocialParticipant(social, account));
-		social.updateDetail(detail);
+    socialParticipantRepository.save(new SocialParticipant(social, account));
+    social.updateDetail(detail);
 
-		return new SocialCreateResponse(social.getId(), "모임을 성공적으로 생성하였습니다.");
-	}
+    return new SocialCreateResponse(social.getId(), "모임을 성공적으로 생성하였습니다.");
+  }
 
-	@Transactional
-	public void updateSocial(final Long id, final SocialUpdateRequest dto) {
-		Account account = getCurrentAccount(accountRepository);
-		Social social =
-				socialRepository
-						.findById(id)
-						.orElseThrow(() -> new CustomException(ErrorDetails.SOCIAL_NOT_FOUND));
-		log.info("어스  이름:", account.getName());
-		log.info("소셜 작성자 이름:", social.getName());
-		if (!account.getName().equals(social.getOwner().getName())) {
-			throw new CustomException(ErrorDetails.SOCIAL_FORBIDDEN);
-		}
+  @Transactional
+  public void updateSocial(final Long id, final SocialUpdateRequest dto) {
+    Account account = getCurrentAccount(accountRepository);
+    Social social =
+        socialRepository
+            .findById(id)
+            .orElseThrow(() -> new CustomException(ErrorDetails.SOCIAL_NOT_FOUND));
+    if (!account.getName().equals(social.getOwner().getName())) {
+      throw new CustomException(ErrorDetails.SOCIAL_FORBIDDEN);
+    }
 
-		social.update(dto);
-	}
+    social.update(dto);
+  }
 
-	@Transactional
-	public void cancelSocial(Long id) {
-		Account account = getCurrentAccount(accountRepository);
-		Social social =
-				socialRepository
-						.findById(id)
-						.orElseThrow(() -> new CustomException(ErrorDetails.SOCIAL_NOT_FOUND));
+  @Transactional
+  public void cancelSocial(Long id) {
+    Account account = getCurrentAccount(accountRepository);
+    Social social =
+        socialRepository
+            .findById(id)
+            .orElseThrow(() -> new CustomException(ErrorDetails.SOCIAL_NOT_FOUND));
 
-		if (!account.getName().equals(social.getOwner().getName())) {
-			throw new CustomException(ErrorDetails.SOCIAL_FORBIDDEN);
-		}
+    if (!account.getName().equals(social.getOwner().getName())) {
+      throw new CustomException(ErrorDetails.SOCIAL_FORBIDDEN);
+    }
 
-		social.cancel();
-	}
+    social.cancel();
+  }
 
-	@Transactional
-	public SocialDetailResponse selectDetailBySocialId(final Long id) {
-		Social social =
-				socialRepository
-						.findById(id)
-						.orElseThrow(() -> new CustomException(ErrorDetails.SOCIAL_NOT_FOUND));
-		SocialDetail detail =
-				socialDetailRepository
-						.findBySocialId(id)
-						.orElseThrow(() -> new CustomException(ErrorDetails.SOCIAL_NOT_FOUND));
+  @Transactional
+  public SocialDetailResponse selectDetailBySocialId(final Long id) {
+    Social social =
+        socialRepository
+            .findById(id)
+            .orElseThrow(() -> new CustomException(ErrorDetails.SOCIAL_NOT_FOUND));
+    SocialDetail detail =
+        socialDetailRepository
+            .findBySocialId(id)
+            .orElseThrow(() -> new CustomException(ErrorDetails.SOCIAL_NOT_FOUND));
 
-		return SocialDetailResponse.fromEntities(social, detail);
-	}
+    return SocialDetailResponse.fromEntities(social, detail);
+  }
 
-	private List<Social> findAllSocials(final String filterBy, final String orderBy) {
-		LocalDateTime now = LocalDateTime.now();
-		String filter = (filterBy == null) ? "" : filterBy;
-		String order = (orderBy == null) ? "" : orderBy;
+  private List<Social> findAllSocials(final String filterBy, final String orderBy) {
+    LocalDateTime now = LocalDateTime.now();
+    String filter = (filterBy == null) ? "" : filterBy;
+    String order = (orderBy == null) ? "" : orderBy;
 
-		switch (filter) {
-			case "open":
-				return socialRepository.findAllByGatheringDateAfterOrderByGatheringDateDesc(now);
-			case "close":
-				return socialRepository.findAllByGatheringDateBeforeOrderByGatheringDateDesc(now);
-			case "cancel":
-				return socialRepository.findAllByCanceledTrueOrderByCreatedAtDesc();
-			case "host":
-				return socialRepository.findAllOrderByPopularityDesc();
-			default:
-				if (order.equals("popularity")) {
-					return socialRepository.findAllOrderByPopularityDesc();
-				}
-				return socialRepository.findAllByOrderByCreatedAtDesc();
-		}
-	}
+    // TODO
+    switch (filter) {
+      case "open":
+        return socialRepository.findAllByGatheringDateAfterOrderByCreatedAtDesc(now);
+      case "close":
+        return socialRepository.findAllByGatheringDateBeforeOrderByCreatedAtDesc(now);
+      case "cancel":
+        return socialRepository.findAllByCanceledTrueOrderByCreatedAtDesc();
+        // TODO 호스트는 API 다르게 가져가야 함
+        //      case "host":
+        //        return socialRepository.findAllByOrderByPopularityDesc();
+      default:
+        if (order.equals("popularity")) {
+          return socialRepository.findAllByOrderByPopularityDesc();
+        }
+        return socialRepository.findAllByOrderByCreatedAtDesc();
+    }
+  }
+
+  //  private List<Social> findAllSocialsByIdList(
+  //      final String filterBy, final String orderBy, final List<Long> ids) {
+  //    LocalDateTime now = LocalDateTime.now();
+  //    String filter = (filterBy == null) ? "" : filterBy;
+  //    String order = (orderBy == null) ? "" : orderBy;
+  //
+  //    switch (filter) {
+  //      case "open":
+  //        return socialRepository.findAllByIdInAndGatheringDateAfterOrderByCreatedAtDesc(ids,
+  // now);
+  //      case "close":
+  //        return socialRepository.findAllByIdInAndGatheringDateBeforeOrderByCreatedAtDesc(ids,
+  // now);
+  //      case "cancel":
+  //        // TODO 추후 고려
+  //      default:
+  //        if (order.equals("popularity")) {
+  //          return socialRepository.findAllByOrderByPopularityDesc();
+  //        }
+  //        return socialRepository.findAllByIdInOrderByCreatedAtDesc(ids);
+  //    }
+  //  }
 }
