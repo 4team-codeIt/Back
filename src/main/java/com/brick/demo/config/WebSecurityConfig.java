fea96adb20a8c3038fe501a6c0fd0bcdec098b70
kimyu0218 <serena35@ewhain.net>
2024-08-05 14:51:46 +0900

    fix: 403 해결

--- src/main/java/com/brick/demo/config/WebSecurityConfig.java
+++ src/main/java/com/brick/demo/config/WebSecurityConfig.java
@@ -21,85 +21,87 @@ import org.springframework.web.filter.CorsFilter;
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-	private final CorsFilter corsFilter;
-	private final TokenProvider tokenProvider;
-	private final List<Map<String, String>> excludeUrlsJwtfilter = List.of(
-			Map.of("url", "/h2-console/**", "method", "ALL"),
-			Map.of("url", "/swagger-ui/**", "method", "ALL"),
-			Map.of("url", "/v3/api-docs/**", "method", "ALL"),
-			Map.of("url", "/auth/signup", "method", "POST"),
-			Map.of("url", "/auth/signin", "method", "POST"),
-			Map.of("url", "/auth/users/duplicate-email", "method", "POST"),
-			Map.of("url", "/auth/users/duplicate-name", "method", "POST"),
-			Map.of("url", "/socials", "method", "GET"),
-			Map.of("url", "/socials/*/details", "method", "GET"),
-			Map.of("url", "/socials/*/qnas", "method", "GET"),
-			Map.of("url", "/socials/*/qnas/*", "method", "GET"),
-			Map.of("url", "/socials/*/qnas/*/comments", "method", "GET"),
-			Map.of("url", "/socials/*/qnas/*/comments/*", "method", "GET")
-	);
+  private final CorsFilter corsFilter;
+  private final TokenProvider tokenProvider;
+  private final List<Map<String, String>> excludeUrlsJwtfilter =
+      List.of(
+          Map.of("url", "/h2-console/**", "method", "ALL"),
+          Map.of("url", "/swagger-ui/**", "method", "ALL"),
+          Map.of("url", "/v3/api-docs/**", "method", "ALL"),
+          Map.of("url", "/auth/signup", "method", "POST"),
+          Map.of("url", "/auth/signin", "method", "POST"),
+          Map.of("url", "/auth/users/duplicate-email", "method", "POST"),
+          Map.of("url", "/auth/users/duplicate-name", "method", "POST"),
+          Map.of("url", "/socials", "method", "GET"),
+          Map.of("url", "/socials/*/details", "method", "GET"),
+          Map.of("url", "/socials/*/qnas", "method", "GET"),
+          Map.of("url", "/socials/*/qnas/*", "method", "GET"),
+          Map.of("url", "/socials/*/qnas/*/comments", "method", "GET"),
+          Map.of("url", "/socials/*/qnas/*/comments/*", "method", "GET"));
 
-	//POST, PATCH, PUT, DELETE 메서드에서 요구됨. 해당 메서드로 요청 시 여기에 허용 안해주면 jwt 필터까지 가지도 못하고 403 에러 발생
-	private final List<String> excludeUrlsCsrf =
-			List.of(
-					"/auth/users",
-					"/auth/users/images",
-					"/socials",
-					"/socials/*",
-					"/socials/*/images",
-					"/socials/*/qnas",
-					"/socials/*/qnas/*",
-					"/socials/*/qnas/*/comments",
-					"/socials/*/qnas/*/comments/*");
+  // POST, PATCH, PUT, DELETE 메서드에서 요구됨. 해당 메서드로 요청 시 여기에 허용 안해주면 jwt 필터까지 가지도 못하고 403 에러 발생
+  private final List<String> excludeUrlsCsrf =
+      List.of(
+          "/auth/users",
+          "/auth/users/images",
+          "/socials",
+          "/socials/*",
+          "/socials/*/participants",
+          "/socials/*/images",
+          "/socials/*/qnas",
+          "/socials/*/qnas/*",
+          "/socials/*/qnas/*/comments",
+          "/socials/*/qnas/*/comments/*");
 
-	public WebSecurityConfig(CorsFilter corsFilter, TokenProvider tokenProvider) {
-		this.corsFilter = corsFilter;
-		this.tokenProvider = tokenProvider;
-	}
+  public WebSecurityConfig(CorsFilter corsFilter, TokenProvider tokenProvider) {
+    this.corsFilter = corsFilter;
+    this.tokenProvider = tokenProvider;
+  }
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 
-	@Bean
-	public SecurityContextLogoutHandler securityContextLogoutHandler() {
-		return new SecurityContextLogoutHandler();
-	}
+  @Bean
+  public SecurityContextLogoutHandler securityContextLogoutHandler() {
+    return new SecurityContextLogoutHandler();
+  }
 
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests(
-						authorize -> {
-							//method가 all이면 모든 요청에 대해, 아니면 각 요청에 대해서만 인증 열어둠.
-							excludeUrlsJwtfilter.forEach(entry -> {
-								if ("ALL".equalsIgnoreCase(entry.get("method"))) {
-									authorize.requestMatchers(entry.get("url")).permitAll();
-								} else {
-									authorize.requestMatchers(HttpMethod.valueOf(entry.get("method")), entry.get("url"))
-											.permitAll();
-								}
-							});
-							authorize.anyRequest().permitAll();
-						})
-				.formLogin(form -> form.disable())
-				.csrf(
-						csrf -> {
-							excludeUrlsJwtfilter.forEach(entry ->
-									csrf.ignoringRequestMatchers(entry.get("url"))
-							);
-							excludeUrlsCsrf.forEach(csrf::ignoringRequestMatchers);
-						})
-				.headers(
-						headers ->
-								headers.frameOptions(frameOptions -> frameOptions.sameOrigin()) // 동일 출처에서 프레임을 허용
-				)
-				.logout((logout) -> logout.logoutUrl("/auth/signout"));
-		http.addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
-				.addFilterBefore(
-						new JwtRequestFilter(tokenProvider, new AntPathMatcher(), excludeUrlsJwtfilter),
-						UsernamePasswordAuthenticationFilter.class);
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests(
+            authorize -> {
+              // method가 all이면 모든 요청에 대해, 아니면 각 요청에 대해서만 인증 열어둠.
+              excludeUrlsJwtfilter.forEach(
+                  entry -> {
+                    if ("ALL".equalsIgnoreCase(entry.get("method"))) {
+                      authorize.requestMatchers(entry.get("url")).permitAll();
+                    } else {
+                      authorize
+                          .requestMatchers(
+                              HttpMethod.valueOf(entry.get("method")), entry.get("url"))
+                          .permitAll();
+                    }
+                  });
+              authorize.anyRequest().permitAll();
+            })
+        .formLogin(form -> form.disable())
+        .csrf(
+            csrf -> {
+              excludeUrlsJwtfilter.forEach(entry -> csrf.ignoringRequestMatchers(entry.get("url")));
+              excludeUrlsCsrf.forEach(csrf::ignoringRequestMatchers);
+            })
+        .headers(
+            headers ->
+                headers.frameOptions(frameOptions -> frameOptions.sameOrigin()) // 동일 출처에서 프레임을 허용
+            )
+        .logout((logout) -> logout.logoutUrl("/auth/signout"));
+    http.addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
+        .addFilterBefore(
+            new JwtRequestFilter(tokenProvider, new AntPathMatcher(), excludeUrlsJwtfilter),
+            UsernamePasswordAuthenticationFilter.class);
 
-		return http.build();
-	}
+    return http.build();
+  }
 }
