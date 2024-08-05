1fd42a40d1e87c5863b2ce9564db0b7229248624
kimyu0218 <serena35@ewhain.net>
2024-08-05 14:44:48 +0900

    feat: name 검색 기준 추가

--- src/main/java/com/brick/demo/social/repository/SocialRepository.java
+++ src/main/java/com/brick/demo/social/repository/SocialRepository.java
@@ -7,68 +7,118 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SocialRepository extends JpaRepository<Social, Long> {
   // GET /socials
 
-  Page<Social> findAllByOrderByCreatedAtDesc(Pageable pageable);
+  Page<Social> findAllByNameContainingIgnoreCaseOrderByCreatedAtDesc(
+      final String name, final Pageable pageable);
 
-  Page<Social> findAllByOrderByGatheringDateDesc(Pageable pageable); // no usage
+  Page<Social> findAllByNameContainingIgnoreCaseAndGatheringDateBeforeOrderByCreatedAtDesc(
+      final String name, final LocalDateTime date, final Pageable pageable);
 
-  Page<Social> findAllByGatheringDateBeforeOrderByCreatedAtDesc(
-      final LocalDateTime date, Pageable pageable);
+  Page<Social> findAllByNameContainingIgnoreCaseAndGatheringDateAfterOrderByCreatedAtDesc(
+      final String name, final LocalDateTime date, final Pageable pageable);
 
-  Page<Social> findAllByGatheringDateBeforeOrderByGatheringDateDesc(
-      final LocalDateTime date, Pageable pageable); // no usage
+  Page<Social> findAllByCanceledTrueOrderByCreatedAtDesc(final Pageable pageable);
 
-  Page<Social> findAllByGatheringDateAfterOrderByCreatedAtDesc(
-      final LocalDateTime date, Pageable pageable);
+  @Query(
+      "SELECT s FROM Social s WHERE LOWER(s.name) LIKE LOWER(CONCAT('%', :name, '%')) ORDER BY SIZE(s.participants) DESC")
+  Page<Social> findAllByNameContainingIgnoreCaseOrderByPopularityDesc(
+      @Param("name") final String name, final Pageable pageable);
 
-  Page<Social> findAllByGatheringDateAfterOrderByGatheringDateDesc(
-      final LocalDateTime date, Pageable pageable); // no usage
+  default Page<Social> findAllInProgress(
+      final String name, final LocalDateTime date, final Pageable pageable) {
+    return findAllByNameContainingIgnoreCaseAndGatheringDateBeforeOrderByCreatedAtDesc(
+        name, date, pageable);
+  }
 
-  Page<Social> findAllByCanceledTrueOrderByCreatedAtDesc(Pageable pageable);
+  default Page<Social> findAllCompleted(
+      final String name, final LocalDateTime date, final Pageable pageable) {
+    return findAllByNameContainingIgnoreCaseAndGatheringDateAfterOrderByCreatedAtDesc(
+        name, date, pageable);
+  }
 
-  @Query("SELECT s FROM Social s ORDER BY SIZE(s.participants) DESC")
-  Page<Social> findAllByOrderByPopularityDesc(Pageable pageable);
+  default Page<Social> findAllCanceled(final Pageable pageable) {
+    return findAllByCanceledTrueOrderByCreatedAtDesc(pageable);
+  }
+
+  default Page<Social> findAllOrderByPopularity(final String name, final Pageable pageable) {
+    return findAllByNameContainingIgnoreCaseOrderByPopularityDesc(name, pageable);
+  }
+
+  default Page<Social> findAllDefault(final String name, final Pageable pageable) {
+    return findAllByNameContainingIgnoreCaseOrderByCreatedAtDesc(name, pageable);
+  }
 
   // GET /socials with id list
 
-  Page<Social> findAllByIdInOrderByCreatedAtDesc(final List<Long> ids, Pageable pageable);
-
-  Page<Social> findAllByIdInOrderByGatheringDateDesc(
-      final List<Long> ids, Pageable pageable); // no usage
+  Page<Social> findAllByIdInOrderByCreatedAtDesc(final List<Long> ids, final Pageable pageable);
 
   Page<Social> findAllByIdInAndGatheringDateBeforeOrderByCreatedAtDesc(
-      final List<Long> ids, final LocalDateTime date, Pageable pageable);
-
-  Page<Social> findAllByIdInAndGatheringDateBeforeOrderByGatheringDateDesc( // no usage
-      final List<Long> ids, final LocalDateTime date, Pageable pageable);
+      final List<Long> ids, final LocalDateTime date, final Pageable pageable);
 
   Page<Social> findAllByIdInAndGatheringDateAfterOrderByCreatedAtDesc(
-      final List<Long> ids, final LocalDateTime date, Pageable pageable);
-
-  Page<Social> findAllByIdInAndGatheringDateAfterOrderByGatheringDateDesc( // no usage
-      final List<Long> ids, final LocalDateTime date, Pageable pageable);
+      final List<Long> ids, final LocalDateTime date, final Pageable pageable);
 
   @Query("SELECT s FROM Social s WHERE s.id IN :ids ORDER BY SIZE(s.participants) DESC")
-  Page<Social> findAllByIdInOrderByPopularityDesc(final List<Long> ids, Pageable pageable);
+  Page<Social> findAllByIdInOrderByPopularityDesc(final List<Long> ids, final Pageable pageable);
+
+  default Page<Social> findPartInProgress(
+      final List<Long> ids, final LocalDateTime date, final Pageable pageable) {
+    return findAllByIdInAndGatheringDateAfterOrderByCreatedAtDesc(ids, date, pageable);
+  }
+
+  default Page<Social> findPartCompleted(
+      final List<Long> ids, final LocalDateTime date, final Pageable pageable) {
+    return findAllByIdInAndGatheringDateBeforeOrderByCreatedAtDesc(ids, date, pageable);
+  }
+
+  default Page<Social> findPartOrderByPopularity(final List<Long> ids, final Pageable pageable) {
+    return findAllByIdInOrderByPopularityDesc(ids, pageable);
+  }
+
+  default Page<Social> findPartDefault(final List<Long> ids, final Pageable pageable) {
+    return findAllByIdInOrderByCreatedAtDesc(ids, pageable);
+  }
 
   // GET /socials/me
 
   Page<Social> findAllByOwnerEntityIdOrderByCreatedAtDesc(
-      final Long ownerEntityId, Pageable pageable);
+      final Long ownerEntityId, final Pageable pageable);
 
   Page<Social> findAllByOwnerEntityIdAndGatheringDateAfterOrderByCreatedAtDesc(
-      final Long ownerEntityId, final LocalDateTime date, Pageable pageable);
+      final Long ownerEntityId, final LocalDateTime date, final Pageable pageable);
 
   Page<Social> findAllByOwnerEntityIdAndGatheringDateBeforeOrderByCreatedAtDesc(
-      final Long ownerEntityId, final LocalDateTime date, Pageable pageable);
+      final Long ownerEntityId, final LocalDateTime date, final Pageable pageable);
 
   @Query(
       "SELECT s FROM Social s WHERE s.owner.entityId = :ownerEntityId ORDER BY SIZE(s.participants) DESC")
   Page<Social> findAllByOwnerEntityIdOrderByPopularityDesc(
-      final Long ownerEntityId, Pageable pageable);
+      final Long ownerEntityId, final Pageable pageable);
+
+  default Page<Social> findMineInProgress(
+      final Long ownerEntityId, final LocalDateTime date, final Pageable pageable) {
+    return findAllByOwnerEntityIdAndGatheringDateAfterOrderByCreatedAtDesc(
+        ownerEntityId, date, pageable);
+  }
+
+  default Page<Social> findMineCompleted(
+      final Long ownerEntityId, final LocalDateTime date, final Pageable pageable) {
+    return findAllByOwnerEntityIdAndGatheringDateBeforeOrderByCreatedAtDesc(
+        ownerEntityId, date, pageable);
+  }
+
+  default Page<Social> findMineOrderByPopularity(
+      final Long ownerEntityId, final Pageable pageable) {
+    return findAllByOwnerEntityIdOrderByPopularityDesc(ownerEntityId, pageable);
+  }
+
+  default Page<Social> findMineDefault(final Long ownerEntityId, final Pageable pageable) {
+    return findAllByOwnerEntityIdOrderByCreatedAtDesc(ownerEntityId, pageable);
+  }
 }
